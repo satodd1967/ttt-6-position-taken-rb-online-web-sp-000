2e8d933efa51b629eb5fe6022f114553c4446b43
Seth Todd <seth.todd@outlook.com>
2019-11-27 01:45:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return false
-  else board[index] == "X" || "O"
+  else board[index] == "x" || "o"
     return true
 end
 end
