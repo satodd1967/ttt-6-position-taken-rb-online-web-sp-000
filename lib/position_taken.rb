cedce0539f7e2578d112bde0f5b374fe167c451f
Seth Todd <seth.todd@outlook.com>
2019-11-27 01:43:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " "
+  if board[index] == " " || "" || nil
     return false
-  else board[index] == "" || nil
-    return false
-  end
+  else board[index] == "X" "O"
 end
